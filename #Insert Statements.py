cc6463f86850307bfc7f0f392ccb87d9c6e67d47
Trevor Blaszczyk <trevorblazek28@gmail.com>
2025-01-23 04:56:57 +0000

    Tell me about you

---
+++ #Insert Statements.py
@@ -0,0 +1,29 @@
+#IF Statements
+
+#name = input("\nWhat is your name?!   ")
+#print("Howdy " + name + "!")
+
+prompt = ("Tell me a little about yourself and I'll repeat it back.")
+prompt += "\n What is your name?"
+
+#retrieving info
+name = input(prompt)
+print("\nHowdy, " + name)
+age = input("\nHow old are you?\t")
+age = int(age)
+if age >= 18:
+    print("\tYou're old enough to vote!"),
+else:
+    print("\tYou're too young to vote!")
+height = input("\nHow tall are you? (in inches)\t")
+height = int(height)
+if height >= 70:
+    print("\tWoah, do you play basketball?")
+else:
+    print("\tShort fella are you?")
+number = input("What is your favorite number?")
+number = int(number)
+if number % 10 ==0:
+    print("ah, divisible by 10, nice, clean number."),
+else:
+    print("An uglier number, but to each their own.")
