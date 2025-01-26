d24ad5986a1d4884bdabbea9002bfb73bc7b95ed
Trevor Blaszczyk <trevorblazek28@gmail.com>
2025-01-26 02:48:41 +0000

    new edit

--- #While Loop.py
+++ #While Loop.py
@@ -1,11 +1,11 @@
 #While Loop
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number += 1
+#current_number = 1
+#while current_number <= 5:
+    #print(current_number)
+    #current_number += 1
 
 prompt = "\n Tell me something you would like repeated."
-prompt += "\n Enter 'quit' to shut me up."
+prompt += "\n Enter 'quit' to shut me up.\t"
 message = ""
 while message != 'quit':
     message = input(prompt)
