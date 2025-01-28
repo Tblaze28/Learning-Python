5867577221fa8389a012f88dc85a6f1a5326969d
Trevor Blaszczyk <trevorblazek28@gmail.com>
2025-01-28 02:56:12 +0000

    1/27 changes

--- #While Loop.py
+++ #While Loop.py
@@ -4,11 +4,30 @@
     #print(current_number)
     #current_number += 1
 
-prompt = "\n Tell me something you would like repeated."
-prompt += "\n Enter 'quit' to shut me up.\t"
-message = ""
-while message != 'quit':
-    message = input(prompt)
+##prompt = "\n Tell me something you would like repeated."
+#prompt += "\n Enter 'quit' to shut me up.\t"
+#message = ""
+#while message != 'quit':
+    #message = input(prompt)
 
-    if message != 'quit':
-        print(message)
+    #if message != 'quit':
+        #print(message)
+
+prompt = "\n Enter the name of a city you've been to!"
+prompt += "\n'quit' to stop\t"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + " someday!")
+    
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number %2 == 0:
+        continue
+
+    print(current_number)
